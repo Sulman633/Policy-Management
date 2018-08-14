7401263257ff0ffc1b048cd1e7d391ac9f531780
Sulman Qureshi <1559926@NOAM.TCS.com>
2018-08-14 14:55:56 -0400

    first commit

---
+++ src/main/java/com/Policy/PolicyManagement/App.java
@@ -0,0 +1,16 @@
+package com.Policy.PolicyManagement;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
