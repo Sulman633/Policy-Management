3c0bbeda360420552169db7884bc593017fa01fa
Khalid Mohamed <kmohamed@uoguelph.ca>
2018-08-16 16:05:41 -0400

    Changes to JDBC_Connection

---
+++ src/main/java/com/Policy/PolicyManagement/PolicyOperations.java
@@ -0,0 +1,14 @@
+package com.Policy.PolicyManagement;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class PolicyOperations {
+
+	private ResultSet rs;
+	private PreparedStatement pstmt;
+	
+	//Update
+	public void updatePolicy(String policyID, String policyName, int numOfNominees, String typeOfPolicy, String tenure, int sumAssured, String prerequistes) {
+	}
+}
