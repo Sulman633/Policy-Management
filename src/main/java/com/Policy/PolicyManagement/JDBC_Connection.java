7e8201a7dd8d999d0b8157e9b6e262b456b9dbdd
Khalid Mohamed <kmohamed@uoguelph.ca>
2018-08-17 10:07:45 -0400

    Established JDBC Connection class. added jar file. Added One PolicyOperation

--- src/main/java/com/Policy/PolicyManagement/JDBC_Connection.java
+++ src/main/java/com/Policy/PolicyManagement/JDBC_Connection.java
@@ -2,22 +2,19 @@ package com.Policy.PolicyManagement;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class JDBC_Connection {
 
 	private Connection con = null;
 	
-	public void connect(String querystatmnt) throws ClassNotFoundException, SQLException
+	public void connect() throws ClassNotFoundException, SQLException
 	{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		String url = "jdbc:oracle:thin:@192.168.1.59:1521:xe";
+		String url = "jdbc:oracle:thin:@192.168.1.132:1521:xe";
 		String uname = "system";
 		String pwd = "tcs12345";
 		con = DriverManager.getConnection(url, uname, pwd);	
-		System.out.println("registered driver");
 	}
 
 	public void closeConnection() throws SQLException {
