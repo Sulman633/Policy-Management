8302ee192add5a206468a85d6b6c0556e7277471
ahmednavivala <42356614+ahmednavivala@users.noreply.github.com>
2018-08-21 10:43:04 -0400

    Added Method to SearchByAgent

--- src/main/java/com/Policy/PolicyManagement/SearchByAgentLogic.java
+++ src/main/java/com/Policy/PolicyManagement/SearchByAgentLogic.java
@@ -44,4 +44,44 @@ public class SearchByAgentLogic {
 		return arr;
 		
 	}
+	
+	public ArrayList<String> showPolicyDetails(String cusID, String policyID) throws SQLException, ClassNotFoundException{
+		ArrayList<String> policydetails = new ArrayList<String>();
+		
+		/* Connect to database */
+		
+		db.connect();
+		policydetails.add(policyID);
+		
+		PreparedStatement ps1 = db.getCon().prepareStatement("select FIRSTNAME, LASTNAME WHERE CUSTOMER_ID = '"+cusID+"'");
+		ResultSet rs = ps1.executeQuery();
+		
+		String name = rs.getString(1) + rs.getString(2);
+		policydetails.add(name);
+		
+		PreparedStatement ps2 = db.getCon().prepareStatement("select tenure, sum_assured WHERE policy_id = '"+policyID+"'");
+		ResultSet rs1 = ps2.executeQuery();
+		
+		policydetails.add(rs1.getString(1));
+		policydetails.add(rs1.getString(2));
+		
+		PreparedStatement ps3 = db.getCon().prepareStatement("select premium_amount, start_date WHERE policy_id = '"+policyID+"' and customer_id  = '"+cusID+"'");
+		ResultSet rs2 = ps3.executeQuery();
+		
+		policydetails.add(rs2.getString(1));
+		policydetails.add(rs2.getString(2)); /* have to add tenure to start date to make it expiry date*/
+		
+		
+		PreparedStatement ps4 = db.getCon().prepareStatement("select nomineemap.nominee_id from nomineemap inner join policymap on nominee.policy_map_id = policymap.policy_map_id WHERE policymap.policy_id = '"+policyID+"' and policymap.customer_id  = '"+cusID+"'");
+		ResultSet rs3 = ps4.executeQuery();
+		
+		policydetails.add(rs3.getString(1));
+		
+		
+		return policydetails;
+		
+		
+		
+		
+	}
 }
